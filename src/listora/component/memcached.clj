165e534e1fb923f3334286e49b2c1dcf9a57c746
James Reeves <jreeves@weavejester.com>
2014-09-29 16:10:58 +0100

    Initial commit

---
+++ src/listora/component/memcached.clj
@@ -0,0 +1,6 @@
+(ns listora.component.memcached)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/listora/component/memcached_test.clj
@@ -0,0 +1,7 @@
+(ns listora.component.memcached-test
+  (:require [clojure.test :refer :all]
+            [listora.component.memcached :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
