9579c95166494e8f5df32094b670a669445fc3e3
James Reeves <jreeves@weavejester.com>
2014-10-01 18:34:52 +0100

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/memcached-component "0.1.0"
+(defproject listora/memcached-component "0.1.1"
   :description "A component for managing a connection to Memcached"
   :url "https://github.com/listora/memcached-component"
   :license {:name "Eclipse Public License"
